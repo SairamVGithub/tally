d80425f652406ec67eb17f2d6b98c687e9eb48c9
root <root@ubuntu.myguest.virtualbox.org>
2023-06-16 13:31:44 -0400

    tally

---
+++ src/main/java/com/vishnu/tally/TallyApplication.java
@@ -0,0 +1,19 @@
+package com.vishnu.tally;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.vishnu.Calculator;
+
+public class TallyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal = new Calculator();
+		//cal.add();
+		List emptyList = Collections.emptyList();
+		System.out.println(emptyList.size());
+
+	}
+
+}
